4018c768338d14df431a93066f3d787198204d7e
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-12-12 18:17:44 -0600

    select molecule

---
+++ src/components/molecules/Select.tsx
@@ -0,0 +1,44 @@
+// Reusable select component
+// "use client"
+
+import React from 'react';
+import { twMerge as tw } from 'tailwind-merge';
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type SelectProps = {
+  options: Option[];
+  label: string;
+  name: string;
+  // onChange: React.ChangeEventHandler<HTMLSelectElement>;
+};
+
+export function Select({ options, label, name }: SelectProps) {
+  return (
+    <div className={tw(`flex flex-col w-full`)}>
+      <label
+        htmlFor={name}
+        className={tw(`text-sm font-medium text-gray-700 mb-1`)}
+      >
+        {label}
+      </label>
+      <select
+        name={name}
+        id={name}
+        // onChange={onChange}
+        className={tw(
+          `border border-gray-300 bg-gray-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`
+        )}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
