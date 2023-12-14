a05fb0ffa4d8dafaf9b5809efd4e8e1f05ae0f38
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-12-13 19:24:23 -0600

    allow onChange

--- src/components/molecules/Select.tsx
+++ src/components/molecules/Select.tsx
@@ -1,4 +1,4 @@
-// "use client"
+"use client"
 import React from 'react';
 import { twMerge as tw } from 'tailwind-merge';
 
@@ -12,14 +12,15 @@ type SelectProps = {
   label: string;
   name: string;
   defaultValue?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
-export function Select({ options, label, name, defaultValue }: SelectProps) {
+export function Select({ options, label, name, defaultValue, onChange }: SelectProps) {
   return (
     <div className={tw(`flex flex-col w-full`)}>
       <label
         htmlFor={name}
-        className={tw(`text-sm font-medium text-gray-700 mb-1`)}
+        className={tw(`text-sm font-medium text-gray-700 my-5`)}
       >
         {label}
       </label>
@@ -27,6 +28,7 @@ export function Select({ options, label, name, defaultValue }: SelectProps) {
         name={name}
         id={name}
         defaultValue={defaultValue}
+        onChange={onChange}
         className={tw(
           `border border-gray-300 bg-gray-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`
         )}
