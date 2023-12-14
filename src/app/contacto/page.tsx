fee6e463b306ff4aa7cea3b91b658e85029a02ca
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-12-13 19:15:37 -0600

    extra opts

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -6,7 +6,7 @@ import { twMerge as tw } from 'tailwind-merge';
 import { submit } from '../actions/contact-us';
 import Button from '@/components/atoms/Button';
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 type State = { message: string };
 
@@ -28,24 +28,32 @@ export default function Contact() {
   return (
     <main>
       <form action={formAction}>
-        <section className={tw(`bg-white pb-6`)}>
-          <div className={tw(`mx-auto p-4 sm:p-6 lg:p-8`)}>
-            <div className={tw(`container mx-auto px-6 p-6 bg-white`)}>
-              <div className={tw(`mb-16 text-center`)}>
-                {/* show formStatus message */}
-                <p className={tw(`text-black font-semibold leading-none`)}>{formStatus?.message}</p>
+        <section className={tw('bg-white', 'pb-6')}>
+          <div className={tw('mx-auto', 'p-4', 'sm:p-6', 'lg:p-8')}>
+            <div
+              className={tw('container', 'mx-auto', 'px-6', 'p-6', 'bg-white')}
+            >
+              <div className={tw('mb-16', 'text-center')}>
+                {/* add extra select: ¿Pagarías por algo así? Sí/No */}
                 <Select
                   options={[
-                    { label: 'Option 1', value: 'option1' },
-                    { label: 'Option 2', value: 'option2' },
-                    { label: 'Option 3', value: 'option3' },
+                    { label: 'Sí', value: 'yes' },
+                    { label: 'No', value: 'no' },
                   ]}
-                  label='Select an option'
-                  name='option'
-                  // onChange={console.log}
+                  label='¿Pagarías al mes por un servicio así?'
+                  name='wouldPay'
+                />
+                {/* add extra select: How much? */}
+                <Select
+                  options={[
+                    { label: 'Menos de $100', value: 'lessThan100' },
+                    { label: 'Entre $100 y $500', value: 'between100and500' },
+                    { label: 'Más de $500', value: 'moreThan500' },
+                  ]}
+                  label='¿Cuánto pagarías?'
+                  name='howMuch'
                 />
               </div>
-              {/* submit btn */}
               <Button type='submit' pendingMessage='Enviando...'>
                 Contáctanos
               </Button>
